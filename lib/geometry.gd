0da8afe2f9e9edc6d6b0f03cc011c856265b39ef
Dersu Uzala <attila@egri-nagy.hu>
2017-08-03 12:03:21 +0900

    distinguishing between to and from distances

--- lib/geometry.gd
+++ lib/geometry.gd
@@ -1,8 +1,9 @@
 ################################################################################
 # Geometry on Cayley graphs.                                                   #
-# (C) Copyright 2011-2015 Attila Egri-Nagy, Andrew Francis                     #
+# (C) Copyright 2011-2017 Attila Egri-Nagy, Andrew Francis                     #
 ################################################################################
 
 DeclareGlobalFunction("Circle");
 DeclareGlobalFunction("Ball");
+DeclareGlobalFunction("DistanceToSet");
 DeclareGlobalFunction("DistanceFromSet");
